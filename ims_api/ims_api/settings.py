91aef81d6c2bf7526410149d8bedfcd49b8ecaa1
obum <obumanichebe@gmail.com>
2025-01-21 13:19:16 +0100

    Fix: corrected the user serializer file

--- ims_api/ims_api/settings.py
+++ ims_api/ims_api/settings.py
@@ -44,8 +44,6 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt.token_blacklist',
     'django_filters',
     'drf_spectacular',
-
-    
 ]
 
 MIDDLEWARE = [
--- ims_api/users/serializers.py
+++ ims_api/users/serializers.py
@@ -100,7 +100,7 @@ class UserSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = User
-        fields =  ['id', 'username', 'role', 'email', 'password']
+        fields =  ['id', 'username', 'role', 'email', 'password',  'created_at', 'updated_at']
         read_only_fields = ['id',  'created_at', 'updated_at']
         
     def update(self, instance, validated_data:dict):
